0a70402c8b2d2986456d45ab4a92f9bacb0b2ede
smitha <mail2vissmithan@gmail.com>
2023-01-14 18:18:43 +0530

    Added - Hilt and Retrofit

--- app/src/main/java/com/smi/giphy/ui/GiphyActivity.kt
+++ app/src/main/java/com/smi/giphy/ui/GiphyActivity.kt
@@ -1,9 +1,12 @@
-package com.smi.giphy
+package com.smi.giphy.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.smi.giphy.R
+import dagger.hilt.android.AndroidEntryPoint
 
-class MainActivity : AppCompatActivity() {
+@AndroidEntryPoint
+class GiphyActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
