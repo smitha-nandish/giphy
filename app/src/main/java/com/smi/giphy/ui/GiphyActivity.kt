a9138c25a446957718fe70599c4f86486e395802
smitha <mail2vissmithan@gmail.com>
2023-01-16 23:27:36 +0530

    Added - GiphyActivity.kt, TrendingGifsFragment.kt, FavouritesGifFragment.kt

--- app/src/main/java/com/smi/giphy/ui/GiphyActivity.kt
+++ app/src/main/java/com/smi/giphy/ui/GiphyActivity.kt
@@ -1,14 +1,44 @@
 package com.smi.giphy.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+import com.google.android.material.tabs.TabLayoutMediator
 import com.smi.giphy.R
+import com.smi.giphy.databinding.ActivityMainBinding
+import com.smi.giphy.ui.fragments.FavouritesGifFragment
+import com.smi.giphy.ui.fragments.TrendingGifsFragment
+import com.smi.giphy.ui.viewpager.GifsPagerAdapter
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class GiphyActivity : AppCompatActivity() {
+class GiphyActivity : FragmentActivity() {
+    private lateinit var giphyViewModel: GiphyViewModel
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initialiseGifsViewPager()
+    }
+
+    private fun initialiseGifsViewPager() {
+        val gifsPagerAdapter = GifsPagerAdapter(this)
+        gifsPagerAdapter.addFragment(TrendingGifsFragment(), getString(R.string.fragment_trending))
+        gifsPagerAdapter.addFragment(FavouritesGifFragment(), getString(R.string.fragment_favourites))
+
+        binding.pager.adapter = gifsPagerAdapter
+        TabLayoutMediator(binding.tabLayout, binding.pager) { tab, position ->
+            tab.text = gifsPagerAdapter.getPageTitle(position)
+        }.attach()
+    }
+
+    override fun onBackPressed() {
+        if (binding.pager.currentItem == 0) {
+            super.onBackPressed()
+        } else {
+            binding.pager.currentItem = binding.pager.currentItem - 1
+        }
     }
 }
