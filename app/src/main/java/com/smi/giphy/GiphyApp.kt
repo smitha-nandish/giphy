b0be2f52abad42726518fc1c5f7adcb5c9abe045
smitha <mail2vissmithan@gmail.com>
2023-01-24 21:25:27 +0530

    Added Firebase - Analytics, Crashlytics and r8

--- app/src/main/java/com/smi/giphy/GiphyApp.kt
+++ app/src/main/java/com/smi/giphy/GiphyApp.kt
@@ -1,7 +1,13 @@
 package com.smi.giphy
 
 import android.app.Application
+import com.google.firebase.FirebaseApp
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class GiphyApp : Application()
+class GiphyApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        FirebaseApp.initializeApp(this)
+    }
+}
